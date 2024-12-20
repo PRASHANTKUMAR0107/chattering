8b97b10e89f9ddcb2f3aaee18ed236cc5fe9a1a2
Prashant Kumar <prashantkumar010704@gmail.com>
2024-12-20 21:14:34 +0530

    socket.io implemented

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,35 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app = express();
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-// app.get('/',(req,res)=>{
-//     res.send("hello bitch");
-// })
-
 app.use('/api/auth',authRoutes);
 app.use('/api/message',messageRoutes);
 app.use('/api/user',userRoutes);
 
-app.listen(PORT,()=>{
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`server is running on port ${PORT} 🚀`);
 })
